6e654e3ec00c9033898e2b05aff380438fba20e6
amurkhon <akramjonovamurxon02@gmail.com>
2025-04-17 21:14:59 +0900

    fix: develop burak screen based components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,9 +3,11 @@ import "../css/app.css";
 import { Box, Button, Container, Grid, Stack, Typography } from "@mui/material";
 import { RippleBadge } from "./MaterialTheme/styled";
 import { Link, Route, Switch } from "react-router-dom";
-import About from "./screens/About";
-import Home from "./screens/Home";
-import Users from "./screens/Users";
+import {HomePage} from "./screens/homePage";
+import { ProductsPage } from "./screens/productsPage";
+import { UserPage } from "./screens/UserPage";
+import { OrdersPage } from "./screens/ordersPage";
+import { HelpPage } from "./screens/helpPage";
 
 function App() {
   return (
@@ -13,13 +15,19 @@ function App() {
         <nav>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/products">ProductsPage</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/orders">OrdersPage</Link>
             </li>
             <li>
-              <Link to="/users">Users</Link>
+              <Link to="/member-page">UserPager</Link>
+            </li>
+            <li>
+              <Link to={"/help"}>Help</Link>
+            </li>
+            <li>
+              <Link to={"/"}>Home</Link>
             </li>
           </ul>
         </nav>
@@ -27,14 +35,20 @@ function App() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about">
-            <About />
+          <Route path="/products">
+            <ProductsPage />
           </Route>
-          <Route path="/users">
-            <Users />
+          <Route path="/orders">
+            <OrdersPage />
+          </Route>
+          <Route path="/member-page">
+            <UserPage />
+          </Route>
+          <Route path="/help">
+            <HelpPage />
           </Route>
           <Route path="/">
-            <Home />
+            <HomePage />
           </Route>
         </Switch>
       </div>
--- src/app/screens/Home.tsx
+++
@@ -1,7 +0,0 @@
-import { Container } from "@mui/material";
-
-function Home() {
-    return <Container>Home</Container>;
-}
-
-export default Home;
