2fc8a1329e992566945e3811c1b4bff1f59f7ea4
amurkhon <akramjonovamurxon02@gmail.com>
2025-04-19 01:18:12 +0900

    fix: organize header and footer components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,56 +2,41 @@ import React from "react";
 import "../css/app.css";
 import { Box, Button, Container, Grid, Stack, Typography } from "@mui/material";
 import { RippleBadge } from "./MaterialTheme/styled";
-import { Link, Route, Switch } from "react-router-dom";
+import { Link, Route, Switch, useLocation } from "react-router-dom";
 import {HomePage} from "./screens/homePage";
 import { ProductsPage } from "./screens/productsPage";
 import { UserPage } from "./screens/UserPage";
 import { OrdersPage } from "./screens/ordersPage";
 import { HelpPage } from "./screens/helpPage";
+import HomeNavbar from "./components/headers/HomeNavbar";
+import OtherNavbar from "./components/headers/OtherNavbar";
+import Footer from "./components/footer/Footer";
 
 function App() {
+  const location = useLocation();
+  console.log("location: ", location);
   return (
-    <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/products">ProductsPage</Link>
-            </li>
-            <li>
-              <Link to="/orders">OrdersPage</Link>
-            </li>
-            <li>
-              <Link to="/member-page">UserPager</Link>
-            </li>
-            <li>
-              <Link to={"/help"}>Help</Link>
-            </li>
-            <li>
-              <Link to={"/"}>Home</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/products">
-            <ProductsPage />
-          </Route>
-          <Route path="/orders">
-            <OrdersPage />
-          </Route>
-          <Route path="/member-page">
-            <UserPage />
-          </Route>
-          <Route path="/help">
-            <HelpPage />
-          </Route>
-          <Route path="/">
-            <HomePage />
-          </Route>
-        </Switch>
-      </div>
+    <>
+    {location.pathname === "/" ? <HomeNavbar/> : <OtherNavbar/>}
+      <Switch>
+        <Route path="/products">
+          <ProductsPage />
+        </Route>
+        <Route path="/orders">
+          <OrdersPage />
+        </Route>
+        <Route path="/member-page">
+          <UserPage />
+        </Route>
+        <Route path="/help">
+          <HelpPage />
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/app/components/headers/OtherNavbar.tsx
@@ -0,0 +1,3 @@
+export default function OtherNavbar() {
+    return <div>OtherNavbar</div>;
+}
