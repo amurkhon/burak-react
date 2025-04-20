49cd3663831c67b1b4378c222d7df8c3839e8f93
amurkhon <akramjonovamurxon02@gmail.com>
2025-04-20 14:27:06 +0900

    feat: develop header and footer part one

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,3 @@
-import React from "react";
-import "../css/app.css";
-import { Box, Button, Container, Grid, Stack, Typography } from "@mui/material";
-import { RippleBadge } from "./MaterialTheme/styled";
 import { Link, Route, Switch, useLocation } from "react-router-dom";
 import {HomePage} from "./screens/homePage";
 import { ProductsPage } from "./screens/productsPage";
@@ -11,6 +7,8 @@ import { HelpPage } from "./screens/helpPage";
 import HomeNavbar from "./components/headers/HomeNavbar";
 import OtherNavbar from "./components/headers/OtherNavbar";
 import Footer from "./components/footer/Footer";
+import "../css/app.css";
+import "../css/navbar.css";
 
 function App() {
   const location = useLocation();
