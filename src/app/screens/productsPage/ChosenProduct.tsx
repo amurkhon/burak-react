0b10dc670b3df322bc83b54333e633aba1d3687b
amurkhon <akramjonovamurxon02@gmail.com>
2025-04-23 22:28:32 +0900

    feat: develop productPage nested routing system

---
+++ src/app/screens/productsPage/ChosenProduct.tsx
@@ -0,0 +1,7 @@
+import { Container } from "@mui/material";
+
+export default function ChosenProduct() {
+    return (
+        <div>Chosen Product</div>
+    )
+}
---
+++ src/app/screens/productsPage/Products.tsx
@@ -0,0 +1,7 @@
+import { Container } from "@mui/material";
+
+export default function Products() {
+    return (
+        <div>Products</div>
+    )
+}
