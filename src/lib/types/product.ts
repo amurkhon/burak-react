3b706a792e7e8dc6a900a188b0d7da7c2095eb9b
amurkhon <akramjonovamurxon02@gmail.com>
2025-05-12 23:14:54 +0900

    feat: develop AppRootState and HomePage type integrations

---
+++ src/lib/types/product.ts
@@ -0,0 +1,25 @@
+import { ProductCollelction, ProductSize, ProductStatus } from "../enums/product.enum";
+
+export interface Product {
+    _id: string,
+    productStatus: ProductStatus,
+    productCollection: ProductCollelction,
+    productName: String,
+    productPrice: Number,
+    productLeftCount: Number,
+    productSize: ProductSize,
+    productVolume: Number,
+    productDesc?: String,
+    productImages: String[],
+    productViews: Number,
+    createAt: Date,
+    updateAt: Date
+}
+
+export interface ProductInquiry {
+    order: string;
+    page: number;
+    limit: number;
+    productCollection?: ProductCollelction;
+    search?: string;
+}
