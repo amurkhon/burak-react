2b72532b38fc195ce11a2e50700e42fa68c82c0a
amurkhon <akramjonovamurxon02@gmail.com>
2025-05-28 10:15:23 +0900

    fix: modify ProcessOrders and Finishedorders componenets

--- src/app/screens/ordersPage/FinishedOrders.tsx
+++ src/app/screens/ordersPage/FinishedOrders.tsx
@@ -1,28 +1,44 @@
 import { TabPanel } from "@mui/lab";
 import { Box, Button, Container, Stack } from "@mui/material";
+import { retrieveFinishedOrdersPage } from "./selector";
+import { createSelector } from "reselect";
+import { Order, OrderItem } from "../../../lib/types/order";
+import { useSelector } from "react-redux";
+import { Product } from "../../../lib/types/product";
+import { serverApi } from "../../../lib/config";
+
+const finishedOrdersRetriver = createSelector(
+    retrieveFinishedOrdersPage,
+    (finishedOrders) => ({finishedOrders})
+); 
 
 export default function FinishedOrders() {
+    const { finishedOrders } = useSelector(finishedOrdersRetriver);
     return  (
         <TabPanel value={"3"}>
             <Stack>
-                {[1,2].map((ele, index) => {
+                {finishedOrders?.map((order: Order) => {
                     return (
-                        <Box key={index} className={"order-main-box"}>
+                        <Box key={order._id} className={"order-main-box"}>
                             <Box className={"order-box-scroll"}>
-                                {[1, 2].map((ele2, index2) => {
+                                {order?.orderItems.map((orderItem: OrderItem) => {
+                                    const product: Product = order.productData.filter(
+                                        (ele: Product) => orderItem.productId === ele._id
+                                    )[0];
+                                    const imagePath = `${serverApi}/${product.productImages[0]}`
                                     return(
-                                        <Box key={index2} className={"orders-name-price"}>
+                                        <Box key={orderItem._id} className={"orders-name-price"}>
                                             <img
-                                                src={"/img/lavash.webp"}
+                                                src={imagePath}
                                                 className={"order-dish-img"}
                                             />
                                             <p className={"title-dish"}>Lavash</p>
                                             <Box className={"price-box-process"}>
-                                                <p>$9</p>
+                                                <p>${product.productPrice}</p>
                                                 <img src={"/icons/close.svg"} />
-                                                <p>2</p>
+                                                <p>{orderItem.itemQuantity}</p>
                                                 <img src={"/icons/pause.svg"} />
-                                                <p style={{marginLeft: "15px"}}>$18</p>
+                                                <p style={{marginLeft: "15px"}}>${product.productPrice * orderItem.itemQuantity}</p>
                                             </Box>
                                         </Box>
                                     );
@@ -32,23 +48,23 @@ export default function FinishedOrders() {
                             <Box className={"total-price-box-finished"}>
                                 <Box className={"box-total"}>
                                     <p>Product price</p>
-                                    <p>$18</p>
+                                    <p>${order.orderTotal - order.orderDelivery}</p>
                                     <img src={"/icons/plus.svg"} style={{marginLeft: "20px"}} />
                                     <p>Delivery cost</p>
-                                    <p>$2</p>
+                                    <p>${order.orderDelivery}</p>
                                     <img
                                         src={"/icons/pause.svg"}
                                         style={{marginLeft: "20px"}}
                                     />
                                     <p>Total</p>
-                                    <p>$36</p>
+                                    <p>${order.orderTotal}</p>
                                 </Box>
                             </Box>
                         </Box>
                     );
                 })}
 
-                {false && (
+                {!finishedOrders || (finishedOrders.length === 0) && (
                     <Box display={"flex"} flexDirection={"row"} justifyContent={"center"}>
                         <img
                             src={"/icons/noimage-list.svg"}
--- src/app/screens/ordersPage/ProcessOrders.tsx
+++ src/app/screens/ordersPage/ProcessOrders.tsx
@@ -1,29 +1,46 @@
 import { TabPanel } from "@mui/lab";
 import { Box, Button, Container, Stack } from "@mui/material";
 import moment from 'moment'
+import { retrieveProcessOrdersPage } from "./selector";
+import { createSelector } from "reselect";
+import { useSelector } from "react-redux";
+import { Order, OrderItem } from "../../../lib/types/order";
+import { Product } from "../../../lib/types/product";
+import { serverApi } from "../../../lib/config";
+
+const processOrdersRetriver = createSelector(
+    retrieveProcessOrdersPage,
+    (processOrders) => ({processOrders})
+);
 
 export default function ProcessOrders() {
+    const { processOrders } = useSelector(processOrdersRetriver);
+    console.log("processOrders: ", processOrders);
     return  (
         <TabPanel value={"2"}>
             <Stack>
-                {[1,2].map((ele, index) => {
+                {processOrders?.map((order: Order) => {
                     return (
-                        <Box key={index} className={"order-main-box"}>
+                        <Box key={order._id} className={"order-main-box"}>
                             <Box className={"order-box-scroll"}>
-                                {[1, 2].map((ele2, index2) => {
+                                {order?.orderItems?.map((orderItem: OrderItem) => {
+                                    const product: Product = order.productData.filter(
+                                        (ele: Product) => orderItem.productId === ele._id
+                                    )[0];
+                                    const imagePath = `${serverApi}/${product.productImages[0]}`;
                                     return(
-                                        <Box key={index2} className={"orders-name-price"}>
+                                        <Box key={orderItem._id} className={"orders-name-price"}>
                                             <img
-                                                src={"/img/lavash.webp"}
+                                                src={imagePath}
                                                 className={"order-dish-img"}
                                             />
-                                            <p className={"title-dish"}>Lavash</p>
+                                            <p className={"title-dish"}>{product.productName}</p>
                                             <Box className={"price-box-process"}>
-                                                <p>$9</p>
+                                                <p>{product.productPrice}</p>
                                                 <img src={"/icons/close.svg"} />
-                                                <p>2</p>
+                                                <p>{orderItem.itemQuantity}</p>
                                                 <img src={"/icons/pause.svg"} />
-                                                <p style={{marginLeft: "15px"}}>$18</p>
+                                                <p style={{marginLeft: "15px"}}>${product.productPrice * orderItem.itemQuantity}</p>
                                             </Box>
                                         </Box>
                                     );
@@ -33,16 +50,16 @@ export default function ProcessOrders() {
                             <Box className={"total-price-box-process"}>
                                 <Box className={"box-total"}>
                                     <p>Product price</p>
-                                    <p>$18</p>
+                                    <p>${order.orderTotal - order.orderDelivery}</p>
                                     <img src={"/icons/plus.svg"} style={{marginLeft: "20px"}} />
                                     <p>Delivery cost</p>
-                                    <p>$2</p>
+                                    <p>${order.orderDelivery}</p>
                                     <img
                                         src={"/icons/pause.svg"}
                                         style={{marginLeft: "20px"}}
                                     />
                                     <p>Total</p>
-                                    <p>$36</p>
+                                    <p>${order.orderTotal}</p>
                                     <p style={{width: "100px"}}>{moment().format("YY-MM-DD hh:mm")}</p>
                                 </Box>
                                 <Button variant="contained" color="secondary" className={"verify-button"}>
@@ -53,7 +70,7 @@ export default function ProcessOrders() {
                     );
                 })}
 
-                {false && (
+                {!processOrders || (processOrders.length === 0) && (
                     <Box display={"flex"} flexDirection={"row"} justifyContent={"center"}>
                         <img
                             src={"/icons/noimage-list.svg"}
